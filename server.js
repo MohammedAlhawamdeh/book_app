0e2b7f6c372a0e11d40dc5da1633eda8bbb8a249
MohammedAlhawamdeh <hawamdeh9393@gmail.com>
2019-12-30 17:31:15 +0200

    submits

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 
 app.set('view engine','ejs');
-// app.use(express.urlencoded());
+app.use(express.urlencoded());
 app.use(express.static('views'));
 app.use('/public', express.static('public'));
 
@@ -18,9 +18,41 @@ app.get('/',(req,res) => {
     res.render('./pages/index')
 });
 
-app.get('/book', (req,res) =>{
-    res.render('./pages/index');
+app.get('/books',(req,res)=>{
+    const url = `https://www.googleapis.com/books/v1/volumes?q=amman`;
+    superagent.get(url)
+    .then(data =>{
+        console.log(data.body.items);
+        res.render('show',{'show': data.body});
+    });
 });
+
+function Book (data){
+    this.image = data.image;
+    this.title = data.title;
+    this.author = data.author;
+    this.description = data.description
+}
+
+// function getBookData
+
+// fucntion bookHandler(request , response){
+//     let title = request.query['title'];
+//     let author = request.query['authors'];
+
+// }
+
+
+
+app.post('/book', (req,res) =>{
+    // res.render('./pages/index');
+    // console.log(req.body);
+    // res.sendFile('./index.ejs', {root: './public'});
+});
+
+
+
+
 
 
 
